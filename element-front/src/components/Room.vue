8d6e85cacde7a8836e4b7d9910811b55b6f5e28d
Marc Lopez <marclopezgarcia@gmail.com>
2022-12-02 10:55:29 +0100

    Moved the socket lib to global of the project and added some events in room

--- element-front/src/components/Room.vue
+++ element-front/src/components/Room.vue
@@ -1,54 +1,54 @@
 <template>
   <div class="room">
-    
-    <button v-on:click="forceUpdateRoom()">Actualizar</button>
-    <Player
-      :player_number="1"
-      :socket="socket"
-    ></Player>
+    2 Player game:
+    <button v-on:click="startQueueSearch('queue2')">{{queueStatus}}</button>
     <br>
-    <Player
-      :player_number="2"
-    ></Player>
-    
+    <br>
+    <button v-on:click="forceUpdateRoom()">Actualizar</button>
+    <Player :player_number="1" :socket="socket"></Player>
+    <br>
+    <Player :player_number="2"></Player>
+
 
     <div v-if="data_ready == true">
       <BoardVue :board="game.board">
       </BoardVue>
 
       <br>
-      Turn: Player {{game.turn_player.player_number +1}}, Status: 
+      Turn: Player {{ game.turn_player.player_number + 1 }}, Status:
     </div>
+    {{ room }}
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BoardVue from './Board.vue';
-import ClientSocket from '@/sockets/client'
 import Player from './Player.vue';
-
-const socket = ClientSocket.setupSocketConnection();
+import { RoomModel, RoomModelMap } from '@/game/models/room';
+import { JoinQueue, QueueTypes } from '@/sockets/client'
+import { SocketInstance } from '@/main'
 
 export default defineComponent({
   name: 'RoomComponent',
   components: {
     BoardVue,
     Player
-},
+  },
   data() {
     return {
+      queueStatus: "Play!",
+      roomId: "",
       data_ready: false,
       uuid: "",
       game: null,
       user_map_list: new Map(),
-      socket: socket
+      room: new RoomModel(0),
 
     }
   },
   mounted() {
-
-    socket.on("gameUpdate", (data) => {
+    SocketInstance.on("gameUpdate", (data) => {
       console.log(data)
 
       this.uuid = data.room.uuid;
@@ -56,14 +56,29 @@ export default defineComponent({
       this.user_map_list = data.room.user_map_list;
 
       console.log(this.game)
+      const room: RoomModel = new RoomModelMap().toDomain(data.room);
+      Object.assign(this.room, room);
 
       this.data_ready = true;
     })
+
+    SocketInstance.on("gameFound", (roomId: string) => {
+      this.roomId = roomId;
+      this.queueStatus = "In game Room"
+    })
   },
   methods: {
+    startQueueSearch(queue_type: string): void {
+
+      const queueData: JoinQueue = {
+        queue: queue_type as QueueTypes
+      };
+
+      SocketInstance.emit("onQueue", queueData);
+      this.queueStatus = "In queue..."
+    },
     forceUpdateRoom(): void {
-      socket.emit("joinRoom", "test");
-      socket.emit('forceGameUpdate');
+      SocketInstance.emit('forceGameUpdate', "room1");
     },
   }
 })
--- element-front/src/main.ts
+++ element-front/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 //import App from './App.vue'
 import Room from '@/components/Room.vue'
+import ClientSocket from './sockets/client';
 
+export const SocketInstance = ClientSocket.setupSocketConnection();
 createApp(Room).mount('#app')
