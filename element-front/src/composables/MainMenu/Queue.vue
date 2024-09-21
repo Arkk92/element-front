3adbc0c29cab5d8d245001eebe61c7a3949f9bb7
Marc Lopez <marclopezgarcia@gmail.com>
2024-09-21 17:28:53 +0200

    Fixing issue with queue selection where selecting a queue different than 1 vs 1 didn't work

--- element-front/src/composables/MainMenu/Queue.vue
+++ element-front/src/composables/MainMenu/Queue.vue
@@ -12,7 +12,7 @@
     </div>
   </div>
   <PlayMenuModal :isOpen="isMenuOpen" @close="isMenuOpen = false" @start-game="startLookingForGame"
-    @username="handleUsernameChange" :is-competitive="queueStatus == 'Ranked Match'"/>
+    @username="handleUsernameChange" :is-competitive="queueStatus == 'Ranked Match'" />
   <JoinGameModal :isOpen="isJoinGameOpen" :game-found="queueStatus == 'GameFound'" @close="isJoinGameOpen = false"
     @join-game="joinGame" @cancel-search="cancelQueue" />
 </template>
@@ -102,28 +102,25 @@ export default defineComponent({
       this.username = username;
     },
 
+    isOfQueueTypes (keyInput: string): keyInput is QueueTypes {
+      return ['queue2', 'queue3', 'queue4'].includes(keyInput);
+    },
+
     startLookingForGame(numPlayers: number) {
-      switch (numPlayers) {
-        case 2:
-          this.queueType = 'queue2'
-          break;
-        case 3:
-          this.queueType = 'queue3'
-          break;
-        case 4:
-          this.queueType = 'queue4'
-          break;
-        default:
-          this.queueType = 'none';
-          break;
-      }
-      this.isMenuOpen = false;
-      if (this.queueType === 'none') {
+
+      const queueType = `queue${numPlayers}`
+
+      if(!this.isOfQueueTypes(queueType)){
+        this.queueType = 'none'
         this.queueStatus = 'Quick Play';
         return;
+      } else {
+        this.queueType = queueType;
       }
-      if(this.username===''){
-        this.username = 'Guest-' + SocketInstance.id.slice(0,4);
+      this.isMenuOpen = false;
+      
+      if (this.username === '') {
+        this.username = 'Guest-' + SocketInstance.id.slice(0, 4);
       }
       this.isJoinGameOpen = true;
       this.queueStatus = 'Searching game...';
