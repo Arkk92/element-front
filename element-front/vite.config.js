e697100fc57fa9476c4cb05b5168dce23f0314d0
Marc Lopez <marclopezgarcia@gmail.com>
2024-09-23 17:12:29 +0200

    Fixed some issues with ESM building

--- element-front/vite.config.js
+++ element-front/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path';
 
 // https://vitejs.dev/config/
-const path = require("path");
 export default defineConfig({
   plugins: [vue()],
   resolve: {
