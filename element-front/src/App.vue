09ff0da726a2b316116f3da1fb7cc4cfea9f56d3
Marc Lopez <marclopezgarcia@gmail.com>
2024-09-24 16:05:12 +0200

    Added a warning when playing in portrait requesting Landscape mode.

--- element-front/src/App.vue
+++ element-front/src/App.vue
@@ -1,24 +1,61 @@
 <template>
-  <UpperNavBarVue/>
-  <RoomViewVue/>
-  <FooterNavBarVue/>
+  <div id="app">
+    <div v-if="isPortrait" class="landscape-warning">
+      Please rotate your device to landscape mode.
+    </div>
+    <div v-else class="app-content">
+      <UpperNavBarVue/>
+      <RoomViewVue/>
+      <FooterNavBarVue/>
+    </div>
+  </div>
+  
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
 import RoomViewVue from './views/RoomView.vue';
 import UpperNavBarVue from './layouts/UpperNavBar.vue';
 import FooterNavBarVue from './layouts/FooterNavBar.vue';
 
-@Options({
-  components: {
-    UpperNavBarVue,
-    FooterNavBarVue,
-    RoomViewVue,
-  },
-})
-export default class App extends Vue {}
-</script>
+import { ref, onMounted, onBeforeUnmount, defineComponent } from 'vue';
 
-<style>
+export default defineComponent({
+  setup() {
+    const isPortrait = ref(false);
+
+    const checkOrientation = () => {
+      isPortrait.value = window.innerHeight > window.innerWidth;
+    };
+
+    onMounted(() => {
+      checkOrientation();
+      window.addEventListener('resize', checkOrientation);
+    });
+
+    onBeforeUnmount(() => {
+      window.removeEventListener('resize', checkOrientation);
+    });
+
+    return {
+      isPortrait
+    };
+  },
+  components: {
+    RoomViewVue,
+    UpperNavBarVue,
+    FooterNavBarVue
+  }
+});
+</script>
+<style scoped>
+.landscape-warning {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: black;
+  color: white;
+  font-size: 24px;
+  text-align: center;
+}
 </style>
