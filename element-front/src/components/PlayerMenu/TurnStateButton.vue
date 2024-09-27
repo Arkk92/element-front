a4db4848b4b807b2f8ac04cba56621b50b0dfdd4
Marc Lopez <marclopezgarcia@gmail.com>
2024-09-27 12:09:31 +0200

    Added a turn state button to show the current status of the current player

---
+++ element-front/src/components/PlayerMenu/TurnStateButton.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="turn-state-button-group d-flex">
+        <div type="button" class="turn-state-button">
+            <RockButton :button-width="'100%'" :button-height="'100%'" :padding="'0px 0px 0px 0px'" :text="state" />
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import RockButton from '../RockButton.vue';
+
+export default defineComponent({
+    name: 'PlayingButtonGroupComponent',
+    components: {
+        RockButton
+    },
+    props: {
+        state: String
+    },
+    methods: {
+        
+    }
+})
+</script>
+<style scoped>
+.turn-state-button-group {
+
+    width: 100%;
+    height: 100%;
+    max-height: 15%;
+    min-height: 15%;
+
+}
+.turn-state-button {
+  max-width: 100%;
+  min-width: 100%;
+}
+</style>
